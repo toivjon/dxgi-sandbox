53be131b10e0a768fb02c6e788321deaa2f78f0a
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-01-01 23:12:17 +0200

    Added some DXGI specific root stuff for the project.

--- dxgi-factories/main.cpp
+++ dxgi-factories/main.cpp
@@ -1,6 +1,21 @@
+#include <dxgi.h>
 #include <iostream>
+#include <wrl/client.h> // ComPtr
+#include <comdef.h>		// _com_error
+
+#pragma comment(lib, "dxgi.lib")
+
+using namespace Microsoft::WRL;
+
+// a utility to easily catch failed HRESULTS.
+inline void check_hresult(HRESULT result) {
+	if (FAILED(0)) {
+		throw _com_error(result);
+	}
+}
 
 int main() {
-	std::cout << "hello dxgi-factories!" << std::endl;
+	ComPtr<IDXGIFactory> dxgiFactory;
+	check_hresult(CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory)));
 	return 0;
 }
